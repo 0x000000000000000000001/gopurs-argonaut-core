6d0cbcb95eb9403f278ee18e2a42976987722a91
0x1 <kevin.francart3@gmail.com>
2026-07-24 16:05:49 +0200

    chore: sync package.json purescript-npm updates

---
+++ src/Data/Argonaut/Core.go
@@ -0,0 +1,41 @@
+
+
+func FromBoolean(_ any) any {
+	panic("Not implemented: fromBoolean")
+}
+
+func FromNumber(_ any) any {
+	panic("Not implemented: fromNumber")
+}
+
+func FromString(_ any) any {
+	panic("Not implemented: fromString")
+}
+
+func FromArray(_ any) any {
+	panic("Not implemented: fromArray")
+}
+
+func FromObject(_ any) any {
+	panic("Not implemented: fromObject")
+}
+
+func JsonNull() any {
+	panic("Not implemented: jsonNull")
+}
+
+func Stringify(_ any) any {
+	panic("Not implemented: stringify")
+}
+
+func StringifyWithIndent(_ any, _ any) any {
+	panic("Not implemented: stringifyWithIndent")
+}
+
+func _CaseJson(_ any, _ any, _ any, _ any, _ any, _ any, _ any) any {
+	panic("Not implemented: _caseJson")
+}
+
+func _Compare(_ any, _ any, _ any, _ any, _ any) any {
+	panic("Not implemented: _compare")
+}
---
+++ src/Data/Argonaut/Parser.go
@@ -0,0 +1,3 @@
+func _JsonParser(_ any, _ any, _ any) any {
+	panic("Not implemented: _jsonParser")
+}
