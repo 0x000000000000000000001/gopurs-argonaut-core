1ec96d51bfcdb8433a080bc77b9783e43cd6534a
0x1 <kevin.francart3@gmail.com>
2026-08-07 13:36:46 +0200

    chore: fixes and cleanup

---
+++ test/Test/Main.go
@@ -0,0 +1,9 @@
+package Test_Main
+
+var ThisIsNull any = nil
+var ThisIsBoolean any = true
+var ThisIsNumber any = float64(12)
+var ThisIsString any = "foobar"
+var ThisIsArray any = []any{"foo", "bar"}
+var ThisIsObject any = map[string]any{"foo": "bar"}
+var ThisIsInvalidString string = `\ffff`
